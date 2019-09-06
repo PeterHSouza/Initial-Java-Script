47302916108f3ed5fbbdfb07da99d42d04e32183
PeterHSouza <pdh.souzac@gmail.com>
2019-09-05 22:49:09 -0300

    Inicial-Java-Script

--- exercicios-js/Fundamentos/strings.js
+++ exercicios-js/Fundamentos/strings.js
@@ -3,3 +3,14 @@ const escola = "Cod3r"
 console.log(escola.charAt(4))
 console.log(escola.charAt(5))
 console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))// vai retornar o indice "3" de dentro do Cod3r
+
+console.log(escola.substring(1))//conta sem o primeiro caractere
+console.log(escola.substring(0, 3))//conta do 1 caractere e nao conta o terceiro
+
+console.log('Escola ' .concat(escola).concat("!"))
+console.log(escola.replace(3, 'e'))// substitui o local indicado por caractere ou numero escolhido
+console.log(escola.replace(/\d/, 'e'))// substituir todos os digitos pela letra "e"
+console.log(escola.replace(/\w/, 'C'))// substitui so o primeiro caractere pela letra escolhida
+console.log(escola.replace(/\w/g, 'e'))// substitui todos os caracteres pela letra escolhida
+console.log('Ana,Maria,Pedro'.split(',')) //para separar em array
