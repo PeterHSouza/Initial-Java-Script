98b68fda66b914a4c8ea4da6ff2bd1c3011c2a27
PeterHSouza <pdh.souzac@gmail.com>
2019-09-10 23:14:31 -0300

    Initial Files

---
+++ exercicios-js/Fundamentos/nullUndefined.js
@@ -0,0 +1,17 @@
+let valor //nao inicializada
+console.log(valor)
+
+valor = null //ausencia de valor 
+console.log (valor)
+//console.log(valor.toString()) //ERRO!
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produtopreco = 3.50
+console.log(produto)
+
+produto.preco = undefined //evite atribuir undefined
+console.log(!!produto.preco)
+console.log(produto)
--- exercicios-js/Fundamentos/objeto.js
+++ exercicios-js/Fundamentos/objeto.js
@@ -9,5 +9,5 @@ const prod2 = {
     nome: 'Camisa Polo',
     preco: 79.90
     }
-console.log(prod2)
 
+console.log(prod2)
