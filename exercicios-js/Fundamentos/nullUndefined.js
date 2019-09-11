f3bdfdf9acec8032c95944037971b1645003876c
PeterHSouza <pdh.souzac@gmail.com>
2019-09-10 23:25:59 -0300

    Initial Files

--- exercicios-js/Fundamentos/nullUndefined.js
+++ exercicios-js/Fundamentos/nullUndefined.js
@@ -14,4 +14,9 @@ console.log(produto)
 
 produto.preco = undefined //evite atribuir undefined
 console.log(!!produto.preco)
+// delete produto.preco
+console.log(produto)
+
+produto.preco = null //sem preco
+console.log(!!produto.preco)
 console.log(produto)
