cc44040eafde6d9063719ebf54d73d214802fdd7
Pedro Henrique de Souza Cunha <pdh.souzac@gmail.com>
2019-10-26 19:24:16 -0300

    Iticial Files

--- exercicios-js/Fundamentos/strings.js
+++ exercicios-js/Fundamentos/strings.js
@@ -9,9 +9,11 @@ console.log(escola.indexOf('3'))// vai retornar o indice "3" de dentro do Cod3r
 console.log(escola.substring(1))//conta sem o primeiro caractere
 console.log(escola.substring(0, 3))//conta do 1 caractere e nao conta o terceiro
 
-console.log('Escola ' .concat(escola).concat("!"))
+console.log('Escola ' .concat(escola).concat("!"))// concatena ou seja junta os caracteres
+console.log('Escola ' + escola + ("!"))
 console.log(escola.replace(3, 'e'))// substitui o local indicado por caractere ou numero escolhido
 console.log(escola.replace(/\d/, 'e'))// substituir todos os digitos pela letra "e"
 console.log(escola.replace(/\w/, 'C'))// substitui so o primeiro caractere pela letra escolhida
 console.log(escola.replace(/\w/g, 'e'))// substitui todos os caracteres pela letra escolhida
+
 console.log('Ana,Maria,Pedro'.split(',')) //para separar em array
--- exercicios-js/Fundamentos/templateString.js
+++ exercicios-js/Fundamentos/templateString.js
@@ -1,11 +1,12 @@
-const nome = 'Rebeca'
+const nome = ' Rebeca'
 const concatenacao = 'Olá' + nome + '!'
 const template  = `
     Olá 
-    ${nome}!`
+    ${nome}!` //abre com o simbolo da crase
 console.log(concatenacao, template)
 
 //expressoes ...
+
 console.log(`1 + 1 = ${1 + 1}`) // o que estiver entre chaves ele vai tentar interpretar
 
 const up = texto => texto.toUpperCase()
