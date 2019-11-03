99244246cc5220ed292420052ed4a97c90da2750
Pedro Henrique de Souza Cunha <pdh.souzac@gmail.com>
2019-11-03 16:40:36 -0300

    Initial Files

---
+++ exercicios-js/Fundamentos/Funcao2.js
@@ -0,0 +1,13 @@
+// armazenando uma funcao numa variavel
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma (2, 3)
+
+// armazenando uma funcao arrow em uma variavel
+
+const soma= (a, b) => {
+    return a + b
+}
+ console.log(soma(2 , 3))
